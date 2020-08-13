ec9825454c1b0c5a48e5371a7bb577bffceddd07
Ann-Marie <anniedillier@gmail.com>
2020-08-13 00:49:41 -0700

    set timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-var working = alert("welcome");
+var timerEl = document.getElementById('countdown');
+var startBtn = document.getElementById('start');
+
+var highscoreE1 = document.getElementById('hightscore')
+var 
+
+function countdown() {
+    var timeLeft = 25;
+  
+    var timeInterval = setInterval(function(){
+    console.log(timeLeft);
+    timerEl.textContent=(timeLeft);
+    if (timeLeft > 0) {
+    timeLeft--;
+    } else {
+      clearInterval(timeInterval);
+    alert("Time is up");
+    }
+  })
+  };
+
+  function setHighScore() {
+      
+  }
+
+  function showHighScore() {
+    var name = document.querySelector("#name").value;
+    var highscore = document.querySelector("#highscore").value;
+    
+    console.log(highscore);
+    highscoreE1.textContent=(highscore);
+
+
+    localStorage.setItem("name", name);
+    localStorage.setItem("hightscore", highscore);
+    
+  };
+
+  startBtn.onclick = countdown;
+  highscoreE1.onclick = showHighscore;
